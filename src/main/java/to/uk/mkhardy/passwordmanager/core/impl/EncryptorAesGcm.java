0d89298452b9d5d42a7c2d0dfd2c9d85eca9a951
matt <matt@matthew>
2020-10-05 20:22:27 +0100

    Update encryption

---
+++ src/main/java/to/uk/mkhardy/passwordmanager/core/impl/EncryptorAesGcm.java
@@ -0,0 +1,64 @@
+package to.uk.mkhardy.passwordmanager.core.impl;
+
+import javax.crypto.Cipher;
+import javax.crypto.SecretKey;
+import javax.crypto.spec.GCMParameterSpec;
+import java.nio.ByteBuffer;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+
+public class EncryptorAesGcm {
+
+    private static final String ENCRYPT_ALGO = "AES/GCM/NoPadding";
+    private static final int TAG_LENGTH_BIT = 128;
+    private static final int IV_LENGTH_BYTE = 12;
+    private static final int AES_KEY_BIT = 256;
+
+    private static final Charset UTF_8 = StandardCharsets.UTF_8;
+
+    public static byte[] encrypt(byte[] pText, SecretKey secret, byte[] iv) throws Exception {
+
+        Cipher cipher = Cipher.getInstance(ENCRYPT_ALGO);
+        cipher.init(Cipher.ENCRYPT_MODE, secret, new GCMParameterSpec(TAG_LENGTH_BIT, iv));
+        byte[] encryptedText = cipher.doFinal(pText);
+        return encryptedText;
+
+    }
+
+    public static byte[] encryptWithPrefixIV(byte[] pText, SecretKey secret, byte[] iv) throws Exception {
+
+        byte[] cipherText = encrypt(pText, secret, iv);
+
+        byte[] cipherTextWithIv = ByteBuffer.allocate(iv.length + cipherText.length)
+                .put(iv)
+                .put(cipherText)
+                .array();
+        return cipherTextWithIv;
+
+    }
+
+    public static byte[] decrypt(byte[] cText, SecretKey secret, byte[] iv) throws Exception {
+
+        Cipher cipher = Cipher.getInstance(ENCRYPT_ALGO);
+        cipher.init(Cipher.DECRYPT_MODE, secret, new GCMParameterSpec(TAG_LENGTH_BIT, iv));
+        byte[] decryptedBytes = cipher.doFinal(cText);
+        return decryptedBytes;
+
+    }
+
+    public static byte[] decryptWithPrefixIV(byte[] cText, SecretKey secret) throws Exception {
+
+        ByteBuffer bb = ByteBuffer.wrap(cText);
+
+        byte[] iv = new byte[IV_LENGTH_BYTE];
+        bb.get(iv);
+        //bb.get(iv, 0, iv.length);
+
+        byte[] cipherText = new byte[bb.remaining()];
+        bb.get(cipherText);
+
+        return decrypt(cipherText, secret, iv);
+
+    }
+
+}
