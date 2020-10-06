eb1e793012c7d2f744ff2287f5997eac3d56a932
matt <matt@matthew>
2020-10-06 14:30:20 +0100

    Update for Jackson

--- src/main/java/to/uk/mkhardy/passwordmanager/core/beans/impl/Question.java
+++ src/main/java/to/uk/mkhardy/passwordmanager/core/beans/impl/Question.java
@@ -1,9 +1,13 @@
 package to.uk.mkhardy.passwordmanager.core.beans.impl;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public final class Question {
 	private final String questionId;
 
-	public Question(String questionId) {
+	@JsonCreator(mode = JsonCreator.Mode.PROPERTIES)
+	public Question(@JsonProperty("questionId") String questionId) {
 		this.questionId = questionId;
 	}
 
--- src/main/java/to/uk/mkhardy/passwordmanager/core/beans/impl/User.java
+++ src/main/java/to/uk/mkhardy/passwordmanager/core/beans/impl/User.java
@@ -1,9 +1,13 @@
 package to.uk.mkhardy.passwordmanager.core.beans.impl;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public final class User {
 	private final String userName;
-
-	public User(String userName) {
+	
+	@JsonCreator(mode = JsonCreator.Mode.PROPERTIES)
+	public User(@JsonProperty("userName") String userName) {
 		this.userName = userName;
 	}
 
